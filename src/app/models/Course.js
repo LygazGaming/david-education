38192ee4fd079c145c63661f50eb80646ec99f10
Lygaz Gaming <longa4609@gmail.com>
2025-02-21 16:34:10 +0700

    api khóa học

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -2,11 +2,17 @@ import mongoose from "mongoose";
 
 const CourseSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  image: { type: String, required: true },
-  description: { type: String },
-  duration: { type: String },
   price: { type: Number },
-  featured: { type: Boolean, default: false },
+  features: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  isPopular: {
+    type: Boolean,
+    default: false,
+  },
   createdAt: { type: Date, default: Date.now },
 });
 
